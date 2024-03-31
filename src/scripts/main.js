2705b0b0b67a5adfdf6ac5bba673a68cf60cab1e
Ana Alice <anaeanali5@gmail.com>
2024-03-31 17:32:31 -0300

    continuar

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() { // Este evento é acionado quando o DOM (Document Object Model) do HTML é completamente carregado
+document.addEventListener('DOMContentLoaded', function(evento) { // Este evento é acionado quando o DOM (Document Object Model) do HTML é completamente carregado
+    evento.preventDefault();
     document.getElementById('form-sorteador').addEventListener('submit', function() { // Seleciona o formulário com o id "form-sorteador" e adiciona um ouvinte de evento para o evento de envio (submit)
         let numeromaximo = document.getElementById('numero-maximo').value; // Seleciona o elemento HTML com o id "numero-maximo" e obtém o valor inserido nele
         numeromaximo = parseInt(numeromaximo); // Converte o valor obtido para um número inteiro usando parseInt
 
         let numeroaleatorio = Math.random() * numeromaximo ; // Gera um número aleatório entre 0 e numeromaximo
-        alert(numeroaleatorio); // Exibe um alerta com o número aleatório gerado
+        numeroaleatorio =Math.floor(numeroaleatorio + 1);
+        document.getElementById('resultado-valor').innerText = numeroaleatorio;
+        document.querySelector('.resultado').style.display = 'block';
     });
 });
